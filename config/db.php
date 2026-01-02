51b26efbba29d041bb60f19c52992e4f2a21afb2
klhp <provoda51@gmail.com>
2026-01-02 15:49:17 +0300

    config db

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=apteka_postgres;port=5432;dbname=apteka_db',
+    'username' => 'adminka', 
+    'password' => 'secret',
+    'charset' => 'utf8',
+];
